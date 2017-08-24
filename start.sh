ee98cd1b4c8e691f73c029c08d6aac8bb6efec5f
Miguel Gagliardo <migag21@yahoo.com.ar>
2017-08-24 17:04:06 -0300

    Adding setuptools

--- start.sh
+++ start.sh
@@ -18,7 +18,7 @@ mkdir $HOME/.go
 
 # AWS CLI
 sudo easy_install pip
-sudo pip install --upgrade --no-cache-dir --ignore-installed awscli pip boto3 botocore virtualenv PyYAML
+sudo pip install --upgrade --no-cache-dir --ignore-installed awscli pip boto3 botocore virtualenv PyYAML setuptools
 
 # Vagrant boxes download
 vagrant box add --provider virtualbox centos/7
