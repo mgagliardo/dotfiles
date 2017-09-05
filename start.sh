95fd2db5e14cb9c1c393f0bfd82fd01d78af19f2
Miguel Gagliardo <migag21@yahoo.com.ar>
2017-09-05 16:31:53 -0300

    Adding xcode and svn

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+#Install Command Line Tools
+xcode-select --install
+
 # Install Brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
