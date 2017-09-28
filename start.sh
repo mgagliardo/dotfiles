968b9927af2eb51a37d727185c154d8ac63c94a7
Miguel Gagliardo <mgagliardo@medallia.com>
2017-09-28 09:40:19 -0700

    Adding typos fixes to start.sh

--- start.sh
+++ start.sh
@@ -31,18 +31,18 @@ sudo easy_install pip
 sudo pip install --upgrade --no-cache-dir --ignore-installed awscli pip boto3 botocore virtualenv PyYAML setuptools
 
 # Get latest Docker images
-docker_images = [ 'alpine:3.6', 'ubuntu:14.04', 'ubuntu:16.04' ]
-for docker_image in docker_images; do
+docker_images=('alpine:3.6' 'ubuntu:14.04' 'ubuntu:16.04')
+for docker_image in $docker_images; do
   docker pull $docker_image;
 done
 
 # vagrant plugins and boxes download
-vagrant_plugins = [ 'vagrant-reload', 'vagrant-cachier' ]
-for vagrant_plugin in vagrant_plugins; do
+vagrant_plugins=('vagrant-reload' 'vagrant-cachier' 'vagrant-vbguest')
+for vagrant_plugin in $vagrant_plugins; do
   vagrant plugin install $vagrant_plugin;
 done
 
-vagrant_images = [ 'ubuntu/trusty64', 'ubuntu/xenial64', 'centos/7' ]
-for vagrant_image in vagrant_images; do
+vagrant_images=('ubuntu/trusty64' 'ubuntu/xenial64' 'centos/7')
+for vagrant_image in $vagrant_images; do
   vagrant box add --provider virtualbox $vagrant_image
 done
